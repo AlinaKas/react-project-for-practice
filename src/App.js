c6cefd8479bb2cb593ac88eac1a831082c3fb168
Alina Kasianova <avkasyanova@gmail.com>
2021-10-12 15:48:42 +0300

    modal added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import "./App.css";
+import './App.css';
+import { LoremIpsum } from 'react-lorem-ipsum';
+import React, { Component } from 'react';
+import Modal from './components/Modal';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+    return (
+      <>
+        <button type="button" onClick={this.toggleModal}>
+          Modal open
+        </button>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h2>This is modal's content as CHILDREN</h2>
+            <LoremIpsum p={1} />
+            <button type="button" onClick={this.toggleModal}>
+              Close
+            </button>
+          </Modal>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
